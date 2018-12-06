ec54bcc3b094faac308b53567f03da6f61e12a87
aliu1212 <alexliu12@gmail.com>
2018-12-06 12:36:36 -0800

    worked on index for models handling requests

--- server/models/index.js
+++ server/models/index.js
@@ -2,14 +2,41 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function () {
+      db.query('SELECT text FROM messages', (err, outcome) => {
+      
+        if(err) throw err;
+        
+        console.log('Outcome:', outcome);
+      });
+    }, // a function which produces all the messages
+    // sql = INSERT INTO messages(text, room, name) FROM (messages.text, messages.username ,messages.roomName)
+    post: function (message) {
+      db.query(`INSERT INTO Messages (id, user, Text, timestamp) 
+                VALUES (null, ${message.username}, ${message.message}, ${Date.now()});`, 
+                (err, outcome) => {
+        if (err) throw err;
+      });
+
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function () {
+      db.query('SELECT id_Users, user FROM Messages', (err, outcome) => {
+        if(err) throw err;
+        
+        console.log('Outcome:', outcome);
+      });
+    },
+    post: function () {
+      db.query(`INSERT INTO Users (id) 
+      VALUES (null);`, 
+      (err, outcome) => {
+        if (err) throw err;
+      });
+    }
   }
 };
 
